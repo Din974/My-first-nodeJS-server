01a603d92c8484bc8e9bff1a61a83c48893970b8
luidgi parinet <luidgi.parinet@epitech.eu>
2021-03-02 16:58:15 +0400

    Workshop

---
+++ back/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const User = require('./models/User');
+
+mongoose.connect('mongodb+srv://Luidgi:din9743890@cluster0.lldcc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+{   useNewUrlParser: true,
+    useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use(bodyParser.json());
+
+const authRoutes = require('./routes/auth');
+
+app.use('/auth', authRoutes);
+
+app.post('/autologin',)
+
+// add your new routes/endpoints here
+
+module.exports = app;
